afdb7b546bbe057d4e7683b880eba5c95b3b4b41
beksultantuleev <beksultan.tuleev@studenti.unitn.it>
2021-01-31 19:03:52 +0100

    kalman

--- droneClass.py
+++ droneClass.py
@@ -17,49 +17,49 @@ class Drone():
         return success
     
     def get_flying_state(self):
-        if self.connected():
-            state = self._drone_id.sensors.flying_state
-            print(state)
-            return state
+        # if self.connected():
+        state = self._drone_id.sensors.flying_state
+        print(state)
+        return state
     
     def get_battery(self):
-        if self.connected():
-            self._drone_id.ask_for_state_update()
-            battery = self._drone_id.sensors.battery
-            print(battery)
-            return battery      
+        # if self.connected():
+        self._drone_id.ask_for_state_update()
+        battery = self._drone_id.sensors.battery
+        print(battery)
+        return battery      
     
     def get_xyz(self):
-        if self.connected():
-            self._drone_id.ask_for_state_update()
-            x = self._drone_id.sensors.quaternion_x
-            y = self._drone_id.sensors.quaternion_y
-            z = self._drone_id.sensors.quaternion_z
-            orientation = {"X":x, "Y":y, "Z":z}
-            print(orientation)
-            return orientation
+        # if self.connected():
+        self._drone_id.ask_for_state_update()
+        x = self._drone_id.sensors.quaternion_x
+        y = self._drone_id.sensors.quaternion_y
+        z = self._drone_id.sensors.quaternion_z
+        orientation = {"X":x, "Y":y, "Z":z}
+        print(orientation)
+        return orientation
     
     def get_altitude(self):
-        if self.connected():
-            self._drone_id.ask_for_state_update()
-            altitude_ts = self._drone_id.sensors.altitude_ts
-            print(altitude_ts)
-            return altitude_ts
+        # if self.connected():
+        self._drone_id.ask_for_state_update()
+        altitude_ts = self._drone_id.sensors.altitude_ts
+        print(altitude_ts)
+        return altitude_ts
 
     
     def take_off(self):
-        if self.connected():
-            self._drone_id.smart_sleep(2)
-            print("Safe take off!")
-            self._drone_id.safe_takeoff(5)
-            self._drone_id.smart_sleep(3)
+        # if self.connected():
+        self._drone_id.smart_sleep(2)
+        print("Safe take off!")
+        self._drone_id.safe_takeoff(5)
+        self._drone_id.smart_sleep(3)
     
     def land(self):
-        if self.connected():
-            print("Landing!")
-            self._drone_id.safe_land(5)
-            self._drone_id.smart_sleep(5)
-            self.disconnect()
+        # if self.connected():
+        print("Landing!")
+        self._drone_id.safe_land(5)
+        self._drone_id.smart_sleep(5)
+        self.disconnect()
 
 
 class ReflexAgent:
@@ -67,10 +67,10 @@ class ReflexAgent:
         pass
 
 mambo = Drone("7A:64:62:66:4B:67")
+mambo.connected()
 # mambo.get_battery()
-# mambo.get_flying_state()
-mambo.get_xyz()
-
+mambo.get_flying_state()
+# mambo.get_xyz()
 # mambo.get_altitude()
-# mambo.get_battery()
+mambo.get_battery()
 mambo.disconnect()
