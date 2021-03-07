a7548218ae6126138de0d1f3fa3b1973e842e3fe
beksultantuleev <beksultan.tuleev@studenti.unitn.it>
2021-03-08 00:03:53 +0100

    path drawing

--- PathDrawing.py
+++ PathDrawing.py
@@ -11,21 +11,25 @@ class PathDrawing:
             self.fig = plt.figure()
             self.ax = plt.axes(projection='3d')
 
-    # def axes_creator(self, direction):
-    #     direction = []
-    #     return direction
+    def open_file(self):
+        with open(self.date) as file:
+            data = json.load(file)
+            return data
+
+    def list_of_objects(self):
+        data = self.open_file()
+        for object in data:
+            print(object)
 
     def draw(self, draw_object, color):
         x = []
         y = []
-        z = []
-
-        with open(self.date) as file:
-            data = json.load(file)
-            for i in data[draw_object]:
-                x.append(i[0])
-                y.append(i[1])
-                z.append(i[2])
+        z = []          
+        data = self.open_file()
+        for i in data[draw_object]:
+            x.append(i[0])
+            y.append(i[1])
+            z.append(i[2])
         if self.in_3d:
             self.ax.plot3D(x, y, z, color)
         else:
@@ -39,8 +43,8 @@ class PathDrawing:
 
 
 if __name__ == "__main__":
-    test = PathDrawing("Mar-06-2021", True)
+    test = PathDrawing("Mar-06-2021", False)
+    # test.list_of_objects()
     test.draw("IMU", "red")
-    test.draw("Kalman", "green")
-
+    test.draw("Kalman", "black")
     test.show()
