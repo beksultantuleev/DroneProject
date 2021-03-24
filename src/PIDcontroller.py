12c853210b8eb32239e2d35d46c5d5197cf09229
Beck Kazama <beksultan.tuleev@studenti.unitn.it>
2021-03-24 16:27:17 +0100

    updated PID

--- src/PIDcontroller.py
+++ src/PIDcontroller.py
@@ -11,23 +11,18 @@ class PIDcontroller:
         # ________________
         self.cmd_input = []
         
-        self.Kp = [2,2,2]
-        self.Ki = [0.2, 0.2, 0.2]
-        self.Kd = [0.25, 0.25, 0.25]
+        self.Kp = np.ones((1, 3))[0] * 7    # [7,7,7]
+        self.Ki =np.ones((1, 3))[0] * 0.2  # [0.2, 0.2, 0.2]
+        self.Kd =np.ones((1, 3))[0] * 0.25  # [0.25, 0.25, 0.25]
         self.sample_time = 60
-        self.prev_values = [0, 0, 0]
+        self.prev_error_values = [0, 0, 0]
         self.max_values = 30
         self.min_values = -30
         self.error = [0, 0, 0]
         self.errsum = [0, 0, 0]
 
-        self.Roll = 20
-        self.Pitch = 20
-        self.Yaw = 0
-        self.Throttle = 20
-
         self.last_time = 0.0001
-        self.now = 0.0000
+
         self.derr = [0, 0, 0]
 
     def set_desired_state(self, desired_state):
@@ -43,9 +38,8 @@ class PIDcontroller:
 
     def pid(self, desired_state):
         self.desired_state = desired_state
-        self.error[0] = self.current_state[0] - self.desired_state[0]
-        self.error[1] = self.current_state[1] - self.desired_state[1]
-        self.error[2] = self.current_state[2] - self.desired_state[2]
+        self.error = list(np.subtract(self.current_state, self.desired_state))
+        # print(f"self erros is >>{self.error}")
         self.now = int(round(time.time() * 1000))
 
         self.timechange = self.now - self.last_time
@@ -54,27 +48,25 @@ class PIDcontroller:
             # print(self.sample_time)
             if (self.last_time != 0):
                 # Integration for Ki
-                self.errsum[0] = self.errsum[0]+(self.error[0]*self.timechange)
-                self.errsum[1] = self.errsum[1]+(self.error[1]*self.timechange)
-                self.errsum[2] = self.errsum[2]+(self.error[2]*self.timechange)
+                if self.timechange >1000:
+                    self.timechange =100
+                    self.errsum = list(np.array(self.errsum) +np.array(self.error)* self.timechange/100)
                 # print(f"errorsum is {self.errsum}")
+                # print(f'error is >{self.error} and timechange is> {self.timechange} errsum is >{self.errsum}')
                 # Derivation for Kd
-                self.derr[0] = (
-                    self.error[0]-self.prev_values[0])/self.timechange
-                self.derr[1] = (
-                    self.error[1]-self.prev_values[1])/self.timechange
-                self.derr[2] = (
-                    self.error[2]-self.prev_values[2])/self.timechange
-                # print(f"derivatives are {self.derr}")
+
+                self.derr = list((np.array(self.error)- np.array(self.prev_error_values))/self.timechange)
+                # print(f"derivatives are {self.derr[1]}")
                 # Calculating output in 30
 
-                self.Pitch = -(self.Kp[0]*self.error[0]) - \
-                    (self.Kd[0]*self.derr[0])
-                self.Roll = -(self.Kp[1]*self.error[1]) + \
-                    (self.Kd[1]*self.derr[1])
-                self.Throttle = -(self.Kp[2]*self.error[2])+(self.Kd[2] *
-                                                             self.derr[2])-(self.errsum[2]*self.Ki[2])
-
+                self.Pitch =-(self.Kp[0]*self.error[0]) - \
+                    (self.Kd[0]*self.derr[0]) -(self.errsum[0]*self.Ki[0]) 
+                # print((self.errsum[0]*self.Ki[0]))
+                # print(f'errorsum is >>{self.errsum} and Ki is >>{self.Ki}, product is {self.errsum[0]*self.Ki[0]}')
+                self.Roll =-(self.Kp[1]*self.error[1]) + \
+                    (self.Kd[1]*self.derr[1]) -(self.errsum[1]*self.Ki[1])
+                self.Throttle = -(self.Kp[2]*self.error[2]) + \
+                    (self.Kd[2]*self.derr[2])-(self.errsum[2]*self.Ki[2])
                 # Checking min and max threshold and updating on true
                 # Throttle Conditions
                 if self.Throttle > 30:
@@ -114,9 +106,7 @@ class PIDcontroller:
                         self.adjusted_cmd.append(i)
                 # print(self.adjusted_cmd)
                 # Updating prev values for all axis
-                self.prev_values[0] = self.error[0]
-                self.prev_values[1] = self.error[1]
-                self.prev_values[2] = self.error[2]
+                self.prev_error_values = self.error
 
             self.last_time = self.now
 
@@ -127,22 +117,22 @@ if __name__ == "__main__":
     mambo = PIDcontroller()
     # ====================
     # mambo.set_current_state([0, 0, 0])
-    # mambo.set_desired_state([-5, 0, 0])
+    # mambo.set_desired_state([1, 0, 0])
     # u = mambo.calculate_cmd_input()
     # print(u)
     # ===================
-    destX = -10
+    destX = -3
     num = 0
     mambo.set_desired_state([destX, 0, 0])
     while num >destX:
 
         mambo.set_current_state([num,0,0])
         u = mambo.calculate_cmd_input()
-        num -=0.1
+        num -=0.5
         time.sleep(0.1)
         print(f"{u} at position>> {num}")
 # ========================================
-    # destY = 2
+    # destY = 3
     # num = 0
     # mambo.set_desired_state([0, destY, 0])
     # while num <destY:
